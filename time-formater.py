b898ad7afd68add492a915051d193d3ecb7378cc
Reyhan <75200483+reycodeart@users.noreply.github.com>
2021-07-15 16:37:52 +0300

    Add files via upload

---
+++ time-formater.py
@@ -0,0 +1,22 @@
+def time_formatter(seconds):
+    
+    mod = seconds % 3600
+    hours = (seconds - mod)/3600
+    mod2 = mod % 60
+    mins = (mod-mod2)/60
+    print(int(hours), "saat, ", int(mins), "dakika, ", mod2, "saniye")
+
+time_formatter(8000)
+
+#--------------------
+
+def time_formatter_2(seconds):
+    
+    hours, rem = divmod(seconds, 3600)
+    minute, rem2 = divmod(rem, 60)
+    print(hours, "saat, ", minute, "dakika, ", rem2, "saniye")
+
+time_formatter_2(8000)
+
+
+
