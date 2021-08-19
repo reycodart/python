7c0e87c8c6ae1700e253c2094db8ac54bae44869
Reyhan <75200483+reycodeart@users.noreply.github.com>
2021-08-19 15:05:45 +0000

    Add files via upload

---
+++ renkli-yazı-yazdırma.py
@@ -0,0 +1,10 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"MUSTAFA KEMAL"+" "+Fore.YELLOW+ Back.BLUE+"ATATÜRK")
+print(Back.CYAN+"ULU ÖNDER")
+print(Fore.RED + Back.GREEN+ "MAREŞAL")
+
+
+
