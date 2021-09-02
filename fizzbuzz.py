cee9b6408ea9b05a51723d9a233080d8ad8fcc9b
Reyhan <75200483+reycodeart@users.noreply.github.com>
2021-09-02 17:04:50 +0300

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+x = range(1, 101)
+
+for y in x:
+    if y % 3 == 0 and y % 5 == 0:
+        print ("Fizz Buzz")
+    elif y % 5 == 0:
+        print ("Buzz")
+    elif y % 3 == 0:
+        print ("Fizz")
+    else:
+        print(y)
+
+        
